d644af658870c69088c848bae8dff799ded797d3
loganDeep <44565120+loganDeep@users.noreply.github.com>
2026-02-01 21:58:51 +0530

    removed details

--- filedata.py
+++ filedata.py
@@ -1,5 +1,5 @@
 def getData():
     return """
-           Harsha is my wife. She is working in CSC. She is from maharashtra. She is born on nov 19 1993. She married deepu on nov,22, 2025.         
+           REV9 is my wife. She is working in IT. She is from maharashtra. She is born on nov 19 19XX. She married DE on nov,XX, 20XX.         
 
    """
